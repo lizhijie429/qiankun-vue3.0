76987637fad308be230a2025dfe4979979e98b37
lizhijie429 <632163606@qq.com>
2021-06-23 23:14:31 +0800

    配置对于jsx的支持

--- sub02/src/views/Home.vue
+++ sub02/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <jsx-demo></jsx-demo>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import JsxDemo from "./JsxDemo";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    JsxDemo,
   },
 });
 </script>
