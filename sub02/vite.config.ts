8156a8b61be7c8b6c4f0876f72677f18571f13af
lizhijie429 <632163606@qq.com>
2022-06-13 19:54:28 +0800

    格式化代码

--- sub02/vite.config.ts
+++ sub02/vite.config.ts
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   server: { port: 3020 },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
