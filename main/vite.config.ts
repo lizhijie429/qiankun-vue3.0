733433fdf7e39753fd1434ae03b779f3fe1c5191
lizhijie429 <632163606@qq.com>
2022-07-06 16:23:19 +0800

    升级依赖，配置vue自动导入常用函数

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  plugins: [vue(), DefineOptions(), vueJsx()],
+  plugins: [
+    vue(),
+    DefineOptions(),
+    vueJsx(),
+    AutoImport({
+      dts: 'src/auto-imports.d.ts',
+      imports: ['vue', 'vue-router']
+    })
+  ],
   server: { port: 3000 },
   resolve: {
     alias: {
