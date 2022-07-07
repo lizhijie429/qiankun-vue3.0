f3a302d0eebdce7495d6ce04f04da8871790a650
lizhijie429 <632163606@qq.com>
2022-07-07 14:19:46 +0800

    格式化代码

--- sub01/src/main.ts
+++ sub01/src/main.ts
@@ -1,51 +1,51 @@
-import { createApp, type App } from "vue";
-import app from "./App.vue";
+import { createApp, type App } from 'vue'
+import app from './App.vue'
 
-import { createPinia } from "pinia";
-import router from "./router";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import { createPinia } from 'pinia'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import {
   renderWithQiankun,
   qiankunWindow,
-  type QiankunProps,
-} from "vite-plugin-qiankun/dist/helper";
+  type QiankunProps
+} from 'vite-plugin-qiankun/dist/helper'
 
-let root: App;
+let root: App
 
 function render(props: QiankunProps) {
-  const { container } = props;
-  root = createApp(app);
-  root.use(createPinia());
-  root.use(router);
-  root.use(ElementPlus, { size: "small" });
+  const { container } = props
+  root = createApp(app)
+  root.use(createPinia())
+  root.use(router)
+  root.use(ElementPlus, { size: 'small' })
   const c = container
-    ? container.querySelector("#app")
-    : document.getElementById("app");
-  root.mount(c);
+    ? container.querySelector('#app')
+    : document.getElementById('app')
+  root.mount(c)
 }
 
-console.log(3333);
+console.log(3333)
 
 renderWithQiankun({
   mount(props) {
-    console.log("vue3sub mount");
-    render(props);
+    console.log('vue3sub mount')
+    render(props)
   },
   bootstrap() {
-    console.log("bootstrap");
+    console.log('bootstrap')
   },
   unmount(props) {
-    console.log("vue3sub unmount=", props);
-    root.unmount();
+    console.log('vue3sub unmount=', props)
+    root.unmount()
   },
   update(props) {
-    console.log("vue3sub update");
-    console.log(props);
-  },
-});
+    console.log('vue3sub update')
+    console.log(props)
+  }
+})
 
 if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-  render({});
+  render({})
 }
