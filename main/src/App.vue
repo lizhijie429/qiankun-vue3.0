05872d6322ad74c7eb61cb4957182ace58fa1b9d
lizhijie429 <632163606@qq.com>
2022-07-07 13:57:17 +0800

    格式化文档

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <router-view> </router-view>
 </template>
-<script setup lang="ts">
-import { getStorage } from '@/utils/storage'
-import { useMenuRouterStore } from '@/stores/menu-router'
-const menuRouterStore = useMenuRouterStore()
-onMounted(() => {
-  menuRouterStore.setCurrentModule(getStorage('currentModule'))
-  menuRouterStore.setCurrentPage(getStorage('currentPage'))
-})
-</script>
+<script setup lang="ts"></script>
 <style>
 @import '@/assets/base.css';
 
